ba72267021f279cd1a4129f3c668047fec1f7f3d
Siva Ganesh <sivadoog0808@gmail.com>
2020-05-03 11:10:53 +0530

    Add files via upload

---
+++ precache-manifest.4b6c724c06fc17b5175e1813f21caedd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f79d7d31ac1e87384360a288555e62e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7135a14e9f91ca56b748",
+    "url": "/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "a30e6b66c6906818d1c5",
+    "url": "/static/js/2.82aebd89.chunk.js"
+  },
+  {
+    "revision": "7135a14e9f91ca56b748",
+    "url": "/static/js/main.c1152b7e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
